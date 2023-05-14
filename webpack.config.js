0147115523870796cf842f17f4a189310483fb3d
Alan <alanl07905@gmail.com>
2023-05-14 11:07:40 -0400

    fixed webpack not working

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./country/scripts/edit.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "edit.js",
+  },
+  plugins: [new Dotenv()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
